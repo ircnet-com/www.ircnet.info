50c8722acdecc26b27f3618bcf9b6a7d4cafe4aa
Patrick <patrick@ircnet.com>
2026-02-07 17:05:17 +0100

    News Filters: ALL / OPEN / SASL

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
   public static CLIS_URL = 'https://clis.ircnet.com/';
-  public static INFOBOT_API_URL = 'http://localhost:9002/api/v2';
+  public static INFOBOT_API_URL = 'https://bot.ircnet.info/api';
 }
